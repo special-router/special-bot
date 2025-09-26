fe2c8ab0215be2893246ea6c982d7d48b30a0dec
bodrich <gedooru@gmail.com>
2025-09-26 14:32:29 +0500

    ....

--- apps/telegram_bot/handlers/faq.py
+++ apps/telegram_bot/handlers/faq.py
@@ -1,15 +1,16 @@
+from django.conf import settings
 from telegram import Update, InputMediaPhoto
 from telegram.ext import ContextTypes
 
 
 async def faq(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     media = [
-        InputMediaPhoto(open("apps/telegram_bot/images/1.jpg", "rb")),
-        InputMediaPhoto(open("apps/telegram_bot/images/2.jpg", "rb")),
-        InputMediaPhoto(open("apps/telegram_bot/images/3.jpg", "rb")),
-        InputMediaPhoto(open("apps/telegram_bot/images/4.jpg", "rb")),
-        InputMediaPhoto(open("apps/telegram_bot/images/5.jpg", "rb")),
-        InputMediaPhoto(open("apps/telegram_bot/images/6.jpg", "rb")),
+        InputMediaPhoto(open(f"{settings.BASE_DIR}/apps/telegram_bot/images/1.jpg", "rb")),
+        InputMediaPhoto(open(f"{settings.BASE_DIR}/apps/telegram_bot/images/2.jpg", "rb")),
+        InputMediaPhoto(open(f"{settings.BASE_DIR}/apps/telegram_bot/images/3.jpg", "rb")),
+        InputMediaPhoto(open(f"{settings.BASE_DIR}/apps/telegram_bot/images/4.jpg", "rb")),
+        InputMediaPhoto(open(f"{settings.BASE_DIR}/apps/telegram_bot/images/5.jpg", "rb")),
+        InputMediaPhoto(open(f"{settings.BASE_DIR}/apps/telegram_bot/images/6.jpg", "rb")),
     ]
 
     await context.bot.send_media_group(
