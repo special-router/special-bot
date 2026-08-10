4298a2c9f56abf13b85b8dfd0c20281e6f790885
special-router <special-router@users.noreply.github.com>
2026-08-10 14:28:18 +0300

    Update beat schedule tests for sync_expiry_times_daily

--- apps/monitoring/test_monitoring.py
+++ apps/monitoring/test_monitoring.py
@@ -96,13 +96,13 @@ class BeatScheduleTests(TestCase):
     def test_monitoring_schedules_are_disabled_by_default(self):
         schedule = self._schedule_keys(monitor_enabled=False, l2_enabled=False)
 
-        self.assertEqual(schedule, {'update_user_vpn_daily'})
+        self.assertEqual(schedule, {'update_user_vpn_daily', 'sync_expiry_times_daily'})
 
     def test_l2_schedule_requires_both_monitoring_flags(self):
         without_l2 = self._schedule_keys(monitor_enabled=True, l2_enabled=False)
         with_l2 = self._schedule_keys(monitor_enabled=True, l2_enabled=True)
 
-        self.assertEqual(without_l2, {'special_monitor_l0', 'special_monitor_l1', 'update_user_vpn_daily'})
+        self.assertEqual(without_l2, {'special_monitor_l0', 'special_monitor_l1', 'update_user_vpn_daily', 'sync_expiry_times_daily'})
         self.assertEqual(with_l2, without_l2 | {'special_monitor_l2'})
 
 
